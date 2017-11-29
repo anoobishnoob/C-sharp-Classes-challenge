cbc3ac2b066bb4aa888272c2ce53480046e9f00b
Samme Qandil <Saqandil@gmail.com>
2017-11-28 19:50:04 -0800

    Add files via upload

---
+++ ChallengeHeroMonsterClassesPart2/Default.aspx.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeHeroMonsterClassesPart2
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Character hero = new Character();
+            hero.Name = "Hero";
+            hero.Health = 35;
+            hero.DamageMaximum = 20;
+            hero.AttackBonus = false;
+
+            Character monster = new Character();
+            monster.Name = "Monster";
+            monster.Health = 21;
+            monster.DamageMaximum = 25;
+            monster.AttackBonus = true;
+
+
+            Dice dice = new Dice();
+
+            // Bonus
+            if (hero.AttackBonus)
+                monster.Defend(hero.Attack(dice));
+            if (monster.AttackBonus)
+                hero.Defend(monster.Attack(dice));
+            while (hero.Health > 0 && monster.Health > 0)
+            {
+                monster.Defend(hero.Attack(dice));
+                hero.Defend(monster.Attack(dice));
+
+
+                printStats(hero);
+                printStats(monster);
+            }
+            displayResult(hero, monster);
+            /*int damage = hero.Attack(dice);
+            monster.Defend(damage);
+
+            damage = monster.Attack(dice);
+            hero.Defend(damage);*/
+
+        }
+
+        private void displayResult(Character opponent1, Character opponent2)
+        {
+            if (opponent1.Health <= 0 && opponent2.Health <= 0)
+                resultLabel.Text += String.Format("<p>Both {0) and {1} have died. </p>", opponent1.Name, opponent2.Name);
+            else if (opponent1.Health <= 0)
+                resultLabel.Text += String.Format("<p> The {0} has died</p>", opponent1.Name);
+            else if (opponent2.Health <= 0)
+                resultLabel.Text += String.Format("<p> The {0} has died</p>", opponent2.Name);
+            else
+                resultLabel.Text += String.Format("Error 404");
+        }
+
+        private void printStats(Character character)
+        {
+            resultLabel.Text += String.Format("<p>Name: {0} - Health: {1} - DamageMaximum: {2} - AttackBonus: {3}</p>",
+                character.Name,
+                character.Health,
+                character.DamageMaximum.ToString(),
+                character.AttackBonus.ToString());
+        }
+
+    }
+
+    class Character
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int DamageMaximum { get; set; }
+        public bool AttackBonus { get; set; }
+
+        public int Attack(Dice dice)
+        {
+            //Random random = new Random();
+            //int damage = random.Next(this.DamageMaximum);
+            dice.Sides = this.DamageMaximum;
+            return dice.Roll();
+        }
+
+        public void Defend(int damage)
+        {
+            this.Health -= damage;
+        }
+    }
+
+    class Dice
+    {
+        public int Sides { get; set; }
+
+        Random random = new Random();
+        public int Roll()
+        {
+            return random.Next(this.Sides);
+        }
+    }
+
+    /*
+    5.  You will create a helper method in the Default class called displayResult()
+    which accepts two input parameters, opponent1 and opponent2. 
+    Depending on their health, determine the winner and print out a winning message using both their names.
+    If they both have health less than or equal to zero, then print out a message that they both died.
+
+
+     */
+}
